14f6d1aaea0eebb565b7c24de659208b80b1bb8e
zeppi <jessopb@gmail.com>
2021-04-12 12:11:26 -0400

    fix posts not showing up on desktop

--- ui/component/publishForm/view.jsx
+++ ui/component/publishForm/view.jsx
@@ -141,7 +141,7 @@ function PublishForm(props: Props) {
       if (mode === PUBLISH_MODES.LIVESTREAM) {
         return enableLivestream;
       }
-      return false;
+      return true;
     }
   });
 
